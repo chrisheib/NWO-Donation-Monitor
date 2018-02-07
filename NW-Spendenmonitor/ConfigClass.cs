6f45e028393abe4da2c7e7c73c718d3c9cd6c6dd
chrisheib <chrisheib@gmx.de>
2018-02-08 00:49:34 +0100

    first try at config

--- NW-Spendenmonitor/ConfigClass.cs
+++ NW-Spendenmonitor/ConfigClass.cs
@@ -8,7 +8,7 @@ namespace NW_Spendenmonitor
 {
     public static class ConfigClass
     {
-        public static string VERSION = "v0.43";
+        public static string VERSION = "v0.51";
         public static int ImportLanguage;
         public static Languages.Language UILanguage;
     }
